ad1a6ac5937b2b4301d404ba84ca16950c12df34
KacperK <kacperkwiatek@onet.pl>
2021-04-21 13:56:48 +0200

    Dodaj pliki projektów.

---
+++ WinFormsApp4/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp4
+{
+    enum CurrentPlayer
+    {
+        Cross,
+        Circle
+    }
+    public partial class Form1 : Form
+    {
+        CurrentPlayer CurrentPlayer;
+        public Form1()
+        {
+            InitializeComponent();
+            CurrentPlayer = CurrentPlayer.Cross;
+            changeLabel();
+        }
+
+        private void Mark(object sender, EventArgs e)
+        {
+            Button senderButton = (Button)sender;
+            if(CurrentPlayer == CurrentPlayer.Cross)
+            {
+                senderButton.Text = "X";
+                CurrentPlayer = CurrentPlayer.Circle;
+            }
+            else
+            {
+                senderButton.Text = "O";
+                CurrentPlayer = CurrentPlayer.Cross;
+            }
+            checkForWinner();
+            changeLabel();
+          
+        }
+        public void changeLabel()
+        {
+            if(CurrentPlayer == CurrentPlayer.Cross)
+            {
+                currentPlayerLabel.Text = "Krzyżyk";
+            }
+            else
+            {
+                currentPlayerLabel.Text = "Kółko";
+            }
+        }
+        public void checkForWinner()
+        {
+            if(String.Compare(tl.Text, cl.Text) == 0 && String.Compare(cl.Text, bl.Text) == 0)
+            {
+                winnerLabel victoryScreen = new winnerLabel();
+                victoryScreenScreen.winner = tl.Text;
+                victoryScreen.Show();
+            }
+        }
+    }
+}
